f3c9decae3baccc0f01c4453c5eafa4397c31df9
paultreanor <treanorpaul9@gmail.com>
2025-02-27 19:06:50 +0000

    Add umami analytics tracking code

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import { Toaster } from "sonner"
+import Script from "next/script"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -18,6 +19,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          id="9dd87958-0c31-4f40-80cf-df8b2d036bc2"
+          src="https://cloud.umami.is/script.js"
+          // Or use strategy="beforeInteractive" or "afterInteractive" if needed
+        />
+      </head>
       <body className={inter.className}>
         {children}
         <Toaster />
