78bbda883ac6682bace5b4274b6764c6a79fd8a3
paultreanor <treanorpaul9@gmail.com>
2025-02-27 03:22:14 +0000

    0.2.0: Use escape encodeURIComponent to encode emojis in URL
    This is a breaking change

--- lib/encodingUtils.ts
+++ lib/encodingUtils.ts
@@ -1,18 +1,26 @@
 import type { Task } from "@/types/types"
 
 function encodeTasks(tasks: Task[]): string {
-  return btoa(JSON.stringify(tasks))
+	const jsonString = JSON.stringify(tasks);
+	
+  // b64 can't handle emojies but encodeURIComponent can
+  // While escape/unescape are deprecated, this combination reliably handles
+  // all Unicode characters while keeping the encoded string relatively compact
+  // compared to just encodeURIComponent and btoa
+	return btoa(unescape(encodeURIComponent(jsonString)));
 }
   
 function decodeTasks(encoded: string): Task[] {
-  try {
-    return JSON.parse(atob(encoded))
-  } catch {
-    return []
-  }
+	try {
+		// Reverse of encoding process
+		const jsonString = decodeURIComponent(escape(atob(encoded)));
+		return JSON.parse(jsonString);
+	} catch {
+		return []
+	}
 }
 
 export {
-  encodeTasks,
-  decodeTasks
+	encodeTasks,
+	decodeTasks
 }
