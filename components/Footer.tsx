b1a50846918662fcbb4e5c850d190c13d4a9dc4e
paultreanor <treanorpaul9@gmail.com>
2025-03-29 12:01:23 +0000

    Only show docs link in footer if not on docs page

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,9 +6,11 @@ export default function Footer() {
 		<footer className="w-full mx-auto p-4 pr-12 mt-12">
 			<div className="w-full flex justify-end text-sm text-dusty-purple">
 				<div className="flex gap-6">
-					<Link href="/docs" className="hover:text-dark-blue-ink transition-colors">
-						Documentation
-					</Link>
+					{window.location.pathname !== "/docs" && (
+						<Link href="/docs" className="hover:text-dark-blue-ink transition-colors">
+							Documentation
+						</Link>
+					)}
 				</div>
 			</div>
 		</footer>
