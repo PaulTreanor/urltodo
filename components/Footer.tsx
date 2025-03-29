dbff6b8c40a19f2dce31dbf035e32138c0be0026
paultreanor <treanorpaul9@gmail.com>
2025-03-29 21:56:53 +0000

    Only show docs link in footer if not on docs page

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,16 @@
+"use client"
 import React from "react"
 import Link from "next/link"
+import { usePathname } from "next/navigation"
 
 export default function Footer() {
+	const pathname = usePathname()
+	
 	return (
 		<footer className="w-full mx-auto p-4 pr-12 mt-12">
 			<div className="w-full flex justify-end text-sm text-dusty-purple">
 				<div className="flex gap-6">
-					{window.location.pathname !== "/docs" && (
+					{pathname !== "/docs" && (
 						<Link href="/docs" className="hover:text-dark-blue-ink transition-colors">
 							Documentation
 						</Link>
